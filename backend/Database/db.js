8b57d3e58ea22e0972cc4a45ccc6b021cce36e7e
Lucky Boy <mi477048@gmail.com>
2021-10-20 23:42:04 -0700

    reduce the mongodb node version to 2.2.2 or later

--- backend/Database/db.js
+++ backend/Database/db.js
@@ -1,12 +1,17 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
- const Connection = async(userName,password) =>{
-   const URL = `mongodb+srv://${userName}:${password}@ecommerce-db.1xlx6.mongodb.net/ECOMMERCE-WEBSITE?retryWrites=true&w=majority`;
-   try {
-    await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true});
-    console.log('Database Connected Succesfully');
-   } catch(error) {
-    console.log('Error: ', error.message);
+const Connection = async (userName, password) => {
+  // const URL = `mongodb+srv://${userName}:${password}@ecommerce-db.1xlx6.mongodb.net/ECOMMERCE-WEBSITE?retryWrites=true&w=majority`;
+
+  const URL = `mongodb://${userName}:${password}@ecommerce-db-shard-00-00.1xlx6.mongodb.net:27017,ecommerce-db-shard-00-01.1xlx6.mongodb.net:27017,ecommerce-db-shard-00-02.1xlx6.mongodb.net:27017/ECOMMERCE-WEBSITE?ssl=true&replicaSet=atlas-bz00t8-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  try {
+    await mongoose.connect(URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("Database Connected Succesfully");
+  } catch (error) {
+    console.log("Error: ", error.message);
   }
-}
-export default Connection
+};
+export default Connection;
--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,28 @@
 import express from "express";
-import dotenv from 'dotenv'
-import Connection from './Database/db.js'
-import defaultData from './default.js'
+import dotenv from "dotenv";
+import Connection from "./Database/db.js";
+import defaultData from "./default.js";
 import bodyParser from "body-parser";
-import cors from 'cors'
+import cors from "cors";
 import router from "./routes/router.js";
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
-app.use(bodyParser.json({extended: true}))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(cors())
-app.use('/',router)
-
-
-
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/", router);
 
 const PORT = 8000;
 const userName = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
-Connection(userName,password)
+ Connection(userName, password);
 
-app.listen(PORT,()=>console.log(`The server is successfully runing on Port ${PORT}`))
+app.listen(PORT, () =>
+  console.log(`The server is successfully runing on Port ${PORT}`)
+);
 
 //import default data to db
 
-defaultData()
-
+defaultData();
